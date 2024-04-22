b4621d39f840b60fd1c3c5963415663ec4190bfa
peningoding <khashaaishaalfikri@gmail.com>
2024-04-22 07:55:39 +0700

    ini push lagi

---
+++ src/routes/index.js
@@ -0,0 +1,62 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import HomeTabs from "./tabs";
+import FavoriteScreen from "../view/screens/home/FavoriteScreen";
+import Poli2 from "../view/screens/poli/Poli2";
+import PortalInformation from "../view/screens/poli/PortalInformasi";
+import KlinikUmum from "../view/screens/poli/KlinikUmum";
+import DoctorScreen from "../view/screens/poli/DoctorScreen";
+import TestingWeb from "../view/screens/web/TestingWeb";
+import LoginScreen from "../view/screens/auth/LoginScreen";
+import PoliInformation from "../view/screens/poli/PoliInformation";
+import { Regist } from "../view/screens/registation/Regist";
+import OnboardingScreen from "../view/screens/home/OnboardingScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  const [pertamaLaunch, setPertamaLaunch] = React.useState(false);
+  React.useEffect(async () => {
+    const appData = AsyncStorage.getItem("pertamaLaunch");
+    console.log(appData);
+    // if (appData == null) {
+    //   setPertamaLaunch(true)
+    //   await AsyncStorage.setItem('pertamaLaunch','false')
+    // } else {
+    //   setPertamaLaunch(false)
+    // }
+  }, []);
+
+  return (
+    pertamaLaunch !== null && (
+      <NavigationContainer>
+        <Stack.Navigator
+          // initialRouteName="Onboarding"
+          screenOptions={{
+            headerShown: false, // Menyembunyikan header secara default
+          }}>
+          {pertamaLaunch && (
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+          )}
+          {/* <Stack.Screen name="Onboarding" component={OnboardingScreen}/> */}
+          <Stack.Screen name="Amino Care" component={HomeTabs} />
+          <Stack.Screen name="Favorites" component={FavoriteScreen} />
+          <Stack.Screen name="Portal Informasi" component={PortalInformation} />
+          <Stack.Screen name="Poli2" component={Poli2} />
+          <Stack.Screen name="Klinik Umum" component={KlinikUmum} />
+          <Stack.Screen name="DoctorScreen" component={DoctorScreen} />
+          <Stack.Screen name="Web View" component={TestingWeb} />
+          <Stack.Screen name="Testing" component={Regist} />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login Screen"
+            component={LoginScreen}
+          />
+          <Stack.Screen name="Poli Information" component={PoliInformation} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  );
+}
---
+++ src/view/screens/auth/LoginScreen.js
@@ -0,0 +1,193 @@
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { KeyboardAvoidingView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import HomeScreen from "../home/HomeScreen";
+import { Ionicons } from "react-native-vector-icons";
+
+const WARNA = { primary: "#0A78E2", white: "#fff" };
+
+const LoginScreen = () => {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+  const [unameError, setUnameError] = useState("");
+  const [uname, setUname] = useState("");
+
+  const navigation = useNavigation();
+
+  const keRegist = () => {
+    navigation.navigate("Signup"); //harus diubah ke halaman pendaftaran
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handlePasswordChange = (text) => {
+    // Cek apakah teks password mengandung karakter khusus
+    const containsSpecialChar = /[!@#$%^&*()_=+\-\[\]{};':"\\|,.<>\/?]/.test(
+      text
+    );
+    if (containsSpecialChar) {
+      setPasswordError("Gak boleh pakai karakter itu");
+    } else {
+      setPasswordError("");
+    }
+    setPassword(text);
+  };
+
+  const handleUsernameChange = (text) => {
+    const onlyAngka = /^[0-9]+$/.test(text);
+    if (!onlyAngka) {
+      setUnameError("Cuma boleh pakai angka");
+    } else {
+      setUnameError("");
+    }
+    setUname(text);
+  };
+
+  const isDisabled = !password || !!passwordError || !unameError || !uname;
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: "center",
+          alignContent: "center",
+        }}>
+        <KeyboardAvoidingView enabled>
+          <View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.judul}>Masuk</Text>
+            </View>
+
+            <View>
+              <TextInput
+                style={[styles.inputan, unameError && styles.inputError]}
+                selectionColor={WARNA.primary}
+                placeholder="No. Handphone/ No. RM"
+                placeholderTextColor={"grey"}
+                autoCapitalize="none"
+                onChangeText={handleUsernameChange}
+              />
+            </View>
+
+            {unameError ? (
+              <Text style={styles.errorText}>{unameError}</Text>
+            ) : null}
+
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <TextInput
+                style={[styles.inputan, passwordError && styles.inputError]}
+                selectionColor={WARNA.primary}
+                placeholder="Password"
+                placeholderTextColor={"grey"}
+                autoCapitalize="none"
+                secureTextEntry={!showPassword}
+                value={password}
+                onChangeText={handlePasswordChange}
+                // keyboardType='password'
+              />
+
+              {/* icon mata */}
+              <View style={{ position: "absolute", right: 10 }}>
+                <TouchableOpacity
+                  style={styles.showHideButton}
+                  onPress={toggleShowPassword}>
+                  <Ionicons name={showPassword ? "eye-off" : "eye"} size={24} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            {passwordError ? (
+              <Text style={styles.errorText}>{passwordError}</Text>
+            ) : null}
+
+            {/* <TouchableOpacity style={styles.showHideButton} onPress={toggleShowPassword}>
+                <Ionicons name={showPassword ? 'eye' : 'eye-off'} size={24}/>
+                <Text style={{ textAlign: 'right' }}>{showPassword ? 'Hide Password' : 'Show Password'}</Text>
+              </TouchableOpacity> */}
+
+            <View
+              style={{ marginBottom: 8, marginTop: 8, alignItems: "center" }}>
+              <ButtonPrimary title="Masuk" disabled={isDisabled} />
+            </View>
+
+            <View style={{ flexDirection: "row" }}>
+              <Text>Belum Punya akun?</Text>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: WARNA.primary,
+                    textDecorationLine: "underline",
+                  }}
+                  onPress={keRegist}>
+                  {" "}
+                  Daftar Sekarang
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  judul: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: WARNA.primary,
+    marginBottom: 32,
+  },
+  inputan: {
+    height: 48,
+    width: 350,
+    borderWidth: 1,
+    padding: 8,
+    margin: 4,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+    borderRadius: 8,
+  },
+  showHideButton: {
+    padding: 10,
+  },
+  tombol: {
+    marginTop: 10,
+    backgroundColor: "#007bff",
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginTop: 2,
+    marginBottom: 8,
+    marginLeft: 5,
+  },
+  inputError: {
+    borderColor: "red",
+  },
+});
+
+export default LoginScreen;
