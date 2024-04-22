5d60fdb2b3763b987c820baca541fef67e532ea9
peningoding <khashaaishaalfikri@gmail.com>
2024-04-22 08:58:01 +0700

    comment onboarding

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,48 +16,48 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
-export default function Routes() {
-  const [pertamaLaunch, setPertamaLaunch] = React.useState(false);
-  React.useEffect(async () => {
-    const appData = AsyncStorage.getItem("pertamaLaunch");
-    console.log(appData);
-    // if (appData == null) {
-    //   setPertamaLaunch(true)
-    //   await AsyncStorage.setItem('pertamaLaunch','false')
-    // } else {
-    //   setPertamaLaunch(false)
-    // }
-  }, []);
+// export default function Routes() {
+//   const [pertamaLaunch, setPertamaLaunch] = React.useState(false);
+//   React.useEffect(async () => {
+//     const appData = AsyncStorage.getItem("pertamaLaunch");
+//     console.log(appData);
+// if (appData == null) {
+//   setPertamaLaunch(true)
+//   await AsyncStorage.setItem('pertamaLaunch','false')
+// } else {
+//   setPertamaLaunch(false)
+// }
+// }, []);
 
-  return (
-    pertamaLaunch !== null && (
-      <NavigationContainer>
-        <Stack.Navigator
-          // initialRouteName="Onboarding"
-          screenOptions={{
-            headerShown: false, // Menyembunyikan header secara default
-          }}
-        >
-          {pertamaLaunch && (
+return (
+  // pertamaLaunch !== null && (
+  <NavigationContainer>
+    <Stack.Navigator
+      // initialRouteName="Onboarding"
+      screenOptions={{
+        headerShown: false, // Menyembunyikan header secara default
+      }}
+    >
+      {/* {pertamaLaunch && (
             <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-          )}
-          {/* <Stack.Screen name="Onboarding" component={OnboardingScreen}/> */}
-          <Stack.Screen name="Amino Care" component={HomeTabs} />
-          <Stack.Screen name="Favorites" component={FavoriteScreen} />
-          <Stack.Screen name="Portal Informasi" component={PortalInformation} />
-          <Stack.Screen name="Poli2" component={Poli2} />
-          <Stack.Screen name="Klinik Umum" component={KlinikUmum} />
-          <Stack.Screen name="DoctorScreen" component={DoctorScreen} />
-          <Stack.Screen name="Web View" component={TestingWeb} />
-          <Stack.Screen name="Testing" component={Regist} />
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login Screen"
-            component={LoginScreen}
-          />
-          <Stack.Screen name="Poli Information" component={PoliInformation} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
-  );
-}
+          )} */}
+      {/* <Stack.Screen name="Onboarding" component={OnboardingScreen}/> */}
+      <Stack.Screen name="Amino Care" component={HomeTabs} />
+      <Stack.Screen name="Favorites" component={FavoriteScreen} />
+      <Stack.Screen name="Portal Informasi" component={PortalInformation} />
+      <Stack.Screen name="Poli2" component={Poli2} />
+      <Stack.Screen name="Klinik Umum" component={KlinikUmum} />
+      <Stack.Screen name="DoctorScreen" component={DoctorScreen} />
+      <Stack.Screen name="Web View" component={TestingWeb} />
+      <Stack.Screen name="Testing" component={Regist} />
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Login Screen"
+        component={LoginScreen}
+      />
+      <Stack.Screen name="Poli Information" component={PoliInformation} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  // )
+);
+// }
