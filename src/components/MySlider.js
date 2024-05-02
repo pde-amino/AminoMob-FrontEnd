91f1473f0a3a8c1d67b6b8046e81a654af347b02
peningoding <khashaaishaalfikri@gmail.com>
2024-05-02 09:05:21 +0700

    update SDK

--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f5f5f5",
   },
   imageCarousel: {
-    width: width,
+    width: "100%",
     height: "100%",
     borderRadius: 10,
   },
