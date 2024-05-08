e0c9086c84008d27b2d1baaf5c73906df6008fff
rajutankoding <yoggaxf22@gmail.com>
2024-05-08 13:41:27 +0700

    Convert .jsx

--- src/components/CustomStyle.js
+++
@@ -1,9 +0,0 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
-import GlobalStyles from "../style/GlobalStyles";
-
-export default function CustomStyle({ children }) {
-  return (
-    <SafeAreaView style={GlobalStyles.safeAreaStyle}>{children}</SafeAreaView>
-  );
-}
