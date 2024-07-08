cc880014d738d842fceaa01ea34603410d7c7dda
peningoding <khashaaishaalfikri@gmail.com>
2024-07-08 12:08:24 +0700

    carousel content

--- src/view/screens/home/HomeScreen.jsx
+++ src/view/screens/home/HomeScreen.jsx
@@ -128,7 +128,8 @@ const HomeScreen = () => {
             />
             <TouchableOpacity
               style={GlobalStyles.btnRedSmall}
-              onPress={darurat}>
+              onPress={darurat}
+            >
               <Text style={GlobalStyles.textButtonSmall}>IGD AMINO</Text>
             </TouchableOpacity>
           </View>
@@ -140,14 +141,15 @@ const HomeScreen = () => {
           flex: 2,
           width: wp(90),
           alignSelf: "center",
-        }}>
+        }}
+      >
         <MySlider />
       </View>
 
       <View style={{ paddingHorizontal: 20 }}>
         <Text style={GlobalStyles.h3}>Menu</Text>
       </View>
-    </>
+    </View>
   );
 
   const ListFooterComponent = () => (
@@ -180,12 +182,14 @@ const HomeScreen = () => {
           height: 150,
           width: "100%",
           position: "absolute",
-        }}>
+        }}
+      >
         <Svg height={hp(45)} width={wp(100)} viewBox="0 0 1440 320">
           <Path
             fill="#0a78e2"
             fill-opacity="1"
-            d="M0,288L48,256C96,224,192,160,288,160C384,160,480,224,576,256C672,288,768,288,864,261.3C960,235,1056,181,1152,144C1248,107,1344,85,1392,74.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></Path>
+            d="M0,288L48,256C96,224,192,160,288,160C384,160,480,224,576,256C672,288,768,288,864,261.3C960,235,1056,181,1152,144C1248,107,1344,85,1392,74.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></Path>
         </Svg>
       </View>
 
@@ -199,7 +203,8 @@ const HomeScreen = () => {
               style={{
                 flexDirection: "row",
                 justifyContent: "center",
-              }}>
+              }}
+            >
               <CardButtonComponent
                 data={{ clinicId: item.kd_poli, nameClinic: item.desc }}
                 icon={item.icon}
