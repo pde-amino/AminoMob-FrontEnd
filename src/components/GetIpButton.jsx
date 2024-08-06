aa45be85570ee470e87e28b707720f6cb4c5764e
Rajutan Koding <yoggaxf22@gmail.com>
2024-08-06 07:13:09 +0700

    GetIP Comunity netInfo

---
+++ src/components/GetIpButton.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { View, Button, Text, StyleSheet } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+
+const GetIPButton = () => {
+  const [ipAddress, setIpAddress] = useState("");
+
+  const getIP = async () => {
+    try {
+      const state = await NetInfo.fetch();
+      const ipLocal = state.details.ipAddress;
+      console.log(ipLocal);
+      setIpAddress(ipLocal);
+    } catch (error) {
+      console.error("Error getting IP address:", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Get IP Address" onPress={getIP} />
+      {ipAddress ? <Text>IP Address: {ipAddress}</Text> : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default GetIPButton;
