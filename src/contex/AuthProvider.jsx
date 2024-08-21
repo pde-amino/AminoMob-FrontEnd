edeb5c596fb9fc749e0f9fef8b4d7265e60adf21
Rajutan Koding <yoggaxf22@gmail.com>
2024-08-21 14:00:53 +0700

    Handle Secure Storage Logout

--- src/contex/AuthProvider.jsx
+++ src/contex/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { createContext, useEffect, useState } from "react";
 import { Alert } from "react-native";
+import * as SecureStore from "expo-secure-store";
 
 const AuthContex = createContext();
 
@@ -10,6 +11,8 @@ const AuthProvider = ({ children }) => {
   const logout = async () => {
     try {
       await AsyncStorage.removeItem("userInfo");
+      await SecureStore.deleteItemAsync("userData");
+
       setAuth(null);
     } catch (error) {
       Alert.alert(
